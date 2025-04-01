560c66ccea296310154b01505947e6fa1423992f
RamenAlmond <raymondwsomething@gmail.com>
2025-04-01 16:01:03 -0700

    Started on world.java and agent.java (isAlive() in agent.java is still unimplemented.)

--- src/simstation/Agent.java
+++ src/simstation/Agent.java
@@ -1,6 +1,94 @@
 package simstation;
 
 import java.io.*;
+import mvc.*;
 
 public abstract class Agent implements Runnable, Serializable{
+    private String agentName;
+    transient protected Thread myThread;
+    private boolean suspended;
+    private boolean stopped;
+    private int xc, yc;
+    private World world;
+
+
+    public Agent(String agentName, World world){
+        this.agentName = agentName;
+        this.world = world;
+        xc = Utilities.rng.nextInt(World.SIZE);
+        yc = Utilities.rng.nextInt(World.SIZE);
+        this.suspended = false;
+        this.stopped = false;
+    }
+    
+    public String getName(){
+        return agentName;
+    }
+
+    public World getWorld(){
+        return world;
+    }
+
+    public int getX(){
+        return xc;
+    }
+
+    public int getY(){
+        return yc;
+    }
+
+    public void setXY(int x, int y){
+        xc = x;
+        yc = y;
+    }
+
+    public void start(){
+        stopped = false;
+        suspended = false;
+        myThread = new Thread(this);
+        myThread.start();
+    }
+
+
+    public void stop(){
+        stopped = true;
+        if(suspended){
+            resume();
+        }
+    }
+
+    public synchronized void pause(){
+        suspended = true;
+    }
+
+    public synchronized void resume(){
+        suspended = false;
+        notify();
+    }
+
+    public abstract void update();
+
+
+    @Override
+    public void run(){
+        while(!stopped){
+            try {
+                synchronized(this){
+                    while(suspended){
+                        wait();
+                    }
+                }
+                update();
+                Thread.sleep(250);
+            } catch (InterruptedException e) { 
+                System.err.println("Error Message");
+            }
+        }
+    }
+
+    public boolean isAlive(){
+        
+    }
+
+    
 }
--- src/simstation/World.java
+++ src/simstation/World.java
@@ -1,6 +1,121 @@
 package simstation;
 
+import java.util.*;
 import mvc.*;
 
 public abstract class World extends Model {
+    public static int SIZE = 500;
+    private int clock = 0;
+    private int alive = 0;
+    private List<Agent> agents;
+    private ObserverAgent observer;
+    private boolean running;
+    private boolean paused;
+
+    public World(){
+        agents = new ArrayList<>();
+        observer = new ObserverAgent();
+        running = false;
+        paused = false;
+    }
+
+    public int getClock(){
+        return clock;
+    }
+
+    public int getAlive(){
+        return alive;
+    }
+
+    public List<Agents> getAgents(){
+        return agents;
+    }
+
+
+    public void addAgent(Agent a){
+        agents.add(a);
+    }
+
+    public void startAgents(){
+        running = true;
+        paused = false;
+        clock = 0;
+        agents.clear();
+        populate();
+        alive = agents.size();
+        for(Agent a : agents){
+            a.start();
+        }
+
+        observer.start();
+        changed();
+    }
+
+    public void stopAgents(){
+        running = false;
+        for(Agent a: agents){
+            a.stop();
+        }
+        observer.stop();
+        changed();
+    }
+
+    public void pauseAgents(){
+        paused = true;
+        for(Agent a: agents){
+            a.pause();
+        }
+        observer.pause();
+        changed();
+    }
+
+    public void resumeAgents(){
+        paused = false;
+        for(Agent a: agents){
+            a.resume();
+        }
+        observer.resume();
+        changed();
+    }
+
+    public abstract void populate();
+    
+
+    public String getStatus(){
+        return "idk what values are here";
+    }
+    
+
+    public void updateStatistics(){
+        clock++;
+        alive = 0;
+        for(Agent a: agents){
+            if(a.isAlive()){
+                alive++;
+            }
+        }
+        changed();
+    }
+    
+    public Agent getNeighbor(Agent caller, int radius){
+        int startIndex = Utilities.rng.nextInt(agents.size());
+        int size = agents.size();
+
+        for(int i = 0; i < size; i++){
+            int index = (startIndex + i) % size; //circular iteration 
+            Agent potentialNeighbor = agents.get(index);
+
+            if(potentialNeighbor != caller){
+                int dx = potentialNeighbor.getX() - caller.getX();
+                int dy = potentialNeighbor.getY() - caller.getY();
+
+                if(Math.sqrt(dx * dx + dy * dy ) <= radius){  //Euclidean
+                    return potentialNeighbor;
+                }
+            }
+        }
+        return null;
+    }
+
+
 }
