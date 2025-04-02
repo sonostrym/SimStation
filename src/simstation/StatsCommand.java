ec97f16d2a85ed1b8500038dd679c69ef5871d0c
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-04-01 23:51:26 -0700

    finished StatsCommand.java

--- src/simstation/StatsCommand.java
+++ src/simstation/StatsCommand.java
@@ -3,4 +3,11 @@ package simstation;
 import mvc.*;
 
 public class StatsCommand extends Command {
+      public StatsCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        ((World)model).updateStatistics();
+    }
 }
