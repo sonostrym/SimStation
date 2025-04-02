0b3cbc2478d21e46e2445a50d294f3386f56e581
RamenAlmond <raymondwsomething@gmail.com>
2025-04-02 00:24:30 -0700

    finished ResumeCommand.java

--- src/simstation/ResumeCommand.java
+++ src/simstation/ResumeCommand.java
@@ -3,4 +3,13 @@ package simstation;
 import mvc.*;
 
 public class ResumeCommand extends Command {
+    public ResumeCommand(Model model) {
+        super(model);
+    }
+
+    
+    @Override
+    public void execute() {
+        ((World)model).resumeAgents();
+    }
 }
