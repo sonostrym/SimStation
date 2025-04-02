f6c20765207f3bce918792741dd7efff4a58b629
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-04-01 23:45:57 -0700

    makeModel and makeEditCommands missing

--- src/simstation/WorldFactory.java
+++ src/simstation/WorldFactory.java
@@ -2,5 +2,35 @@ package simstation;
 
 import mvc.*;
 
-public abstract class WorldFactory implements AppFactory {
+public class WorldFactory implements AppFactory {
+    public Model makeModel() {
+        return null; //fix
+    }
+
+
+    public View makeView(Model m) {
+        return new WorldView((World) m);
+    }
+
+    public String[] getEditCommands() {
+        return new String[] {"Resume", "Suspend", "Start", "Stop", "Stats"};
+    }
+
+    public Command makeEditCommand(Model model, String name, Object source) {
+        return null; //fix
+    }
+
+    public String getTitle() {
+        return "SimStation";
+    }
+
+    public String[] getHelp() {
+        return new String[] {
+                "Click start, stop, pause, or resume to control agent behavior."
+        };
+    }
+
+    public String about() {
+        return "SimStation version 1.0";
+    }
 }
