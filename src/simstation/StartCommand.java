fdbe00eec3a1c095d3f6e60d315e9f659fba83cd
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-04-01 23:50:01 -0700

    finished StartCommand.java

--- src/simstation/StartCommand.java
+++ src/simstation/StartCommand.java
@@ -3,4 +3,12 @@ package simstation;
 import mvc.*;
 
 public class StartCommand extends Command {
+
+  public StartCommand (Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        ((World)model).startAgents();
+    }
 }
