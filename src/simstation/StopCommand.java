84e7245d32ebf21a1230b75d1a5479bd578b7e46
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-04-01 23:50:21 -0700

    finished StopCommand.java

--- src/simstation/StopCommand.java
+++ src/simstation/StopCommand.java
@@ -3,4 +3,11 @@ package simstation;
 import mvc.*;
 
 public class StopCommand extends Command {
+      public StopCommand(Model model) {
+        super(model);
+    }
+
+    public void execute() {
+        ((World)model).stopAgents();
+    }
 }
