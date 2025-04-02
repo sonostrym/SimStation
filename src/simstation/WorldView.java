ae529948e3599e4ae5308aec06d5fd1b1416cc28
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-04-01 23:34:54 -0700

    Update WorldView.java

--- src/simstation/WorldView.java
+++ src/simstation/WorldView.java
@@ -3,4 +3,27 @@ package simstation;
 import mvc.*;
 
 public class WorldView extends View {
+  
+      public WorldView(Model model) {
+        super(model);
+    }
+
+    public void setModel(Model newModel) {
+        super.setModel(newModel);
+        repaint();
+    }
+
+    @Override
+    public void paintComponent(Graphics gc) {
+        super.paintComponent(gc);
+        World world = (World) model;
+        for (Agent a : world.getAgents()) {
+            drawAgent(a, gc);
+        }
+    }
+
+    public void drawAgent(Agent a, Graphics gc) {
+        gc.setColor(Color.RED);
+        gc.fillOval(a.getX(), a.getY(), 10, 10);
+    }
 }
