7baef203163b0939a4652b25ebb8d2bde344b5a2
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-04-08 20:04:41 -0700

    Update Tit4Tat.java

--- src/prisonerdilemma/Tit4Tat.java
+++ src/prisonerdilemma/Tit4Tat.java
@@ -1,4 +1,10 @@
 package prisonerdilemma;
 
 public class Tit4Tat extends Strategy {
+  
+    @Override
+    public boolean cooperate(Prisoner self) {
+        return !self.getPartnerCheated();
+    }
+
 }
