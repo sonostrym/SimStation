67ee26ef002df3eb0c3814d01a9769c92beb9ab1
diyadalal <87991739+diyadalal@users.noreply.github.com>
2025-04-14 14:10:56 -0700

    added view + more in help

--- src/prisonerdilemma/PrisonersDilemmaFactory.java
+++ src/prisonerdilemma/PrisonersDilemmaFactory.java
@@ -6,10 +6,12 @@ import simstation.*;
 public class PrisonersDilemmaFactory extends WorldFactory {
     public Model makeModel() { return new PrisonersDilemmaSimulation(); }
     public String getTitle() { return "Prisoner's Dilemma"; }
+        public View makeView(Model m) { return new PrisonersDilemmaView(m);}
 
     public String[] getHelp() {
         return new String[] {
-                "Click on Stats to see average fitness scores for each strategy."
+                "Click on Stats to see average fitness scores for each strategy." + "\n" +
+                        "The color of the dot (from red to blue) represents how high that prisoners fitness score is."
         };
     }
 
